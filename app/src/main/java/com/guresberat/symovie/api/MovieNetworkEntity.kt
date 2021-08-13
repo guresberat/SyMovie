976d4969b640e14249546c6617a23061f108b96e
guresberatcan <guresberatcan@gmail.com>
2021-08-13 13:08:07 +0300

    Added Movie and MovieNetworkEntity models
    Signed-off-by: Beratcan Güreş <guresberatcan@gmail.com>

---
+++ app/src/main/java/com/guresberat/symovie/api/MovieNetworkEntity.kt
@@ -0,0 +1,59 @@
+package com.guresberat.symovie.api
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class MovieNetworkEntity(
+
+    @SerializedName("budget")
+    @Expose
+    var budget: Int,
+
+    @SerializedName("id")
+    @Expose
+    var id: Int,
+
+    @SerializedName("name")
+    @Expose
+    var name: String,
+
+    @SerializedName("overview")
+    @Expose
+    var overview: String,
+
+    @SerializedName("popularity")
+    @Expose
+    var popularity: Double,
+
+    @SerializedName("poster_path")
+    @Expose
+    val posterPath: String,
+
+    @SerializedName("release_date")
+    @Expose
+    val releaseDate: String,
+
+    @SerializedName("revenue")
+    @Expose
+    val revenue: Long,
+
+    @SerializedName("runtime")
+    @Expose
+    val runtime: Int,
+
+    @SerializedName("status")
+    @Expose
+    val status: String,
+
+    @SerializedName("tagline")
+    @Expose
+    val tagline: String,
+
+    @SerializedName("title")
+    @Expose
+    val title: String,
+
+    @SerializedName("vote_average")
+    @Expose
+    val rating: Double,
+)
